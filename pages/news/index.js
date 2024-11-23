9d63d2d43cf6648bbbb4e25d584547e37f78fff9
Denis Hristov <48285397+DenisHristov12@users.noreply.github.com>
2024-11-23 23:14:52 +0200

    linking between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,19 @@
+import Link from 'next/link';
+
 function NewsPage() {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-framework">NextJS Is A Great Framework</Link>
+        </li>
+        <li>
+          <Link href="/news/else">Something else</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
