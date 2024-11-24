ae7581d36fce00356a7ef5060c389ffb9345d504
Denis Hristov <48285397+DenisHristov12@users.noreply.github.com>
2024-11-24 19:24:48 +0200

    prerender details page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,38 @@ function DetailsPage() {
   );
 }
 
+export function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      MeetupData: {
+        image: '',
+        id: 'm1',
+        title: 'First meetup',
+        address: 'Some Street 5, Some City',
+        descripton: 'This is a first meetup',
+      },
+    },
+  };
+}
+
 export default DetailsPage;
